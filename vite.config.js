36e0ea444a21d6916be5446c5f54013978ae3300
Sneh297 <snehdholia@gmail.com>
2026-05-20 14:22:46 +0530

    Refactor Vite configuration and add Netlify build settings

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
 
 export default defineConfig({
-  plugins: [
-    react(),
-    
-
-
-  ],
-  server: {
-
-
-    allowedHosts: [
- 'relay-upcoming-celebrities-gym.trycloudflare.com'
-    ],
-
-  }
+  base: '/'
 })
