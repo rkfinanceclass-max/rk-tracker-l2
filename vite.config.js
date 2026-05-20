55f07668d351090afae171606bf8782e71c550d5
Sneh297 <snehdholia@gmail.com>
2026-05-20 14:06:56 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+
+export default defineConfig({
+  plugins: [
+    react(),
+    
+
+
+  ],
+  server: {
+
+
+    allowedHosts: [
+ 'relay-upcoming-celebrities-gym.trycloudflare.com'
+    ],
+
+  }
+})
